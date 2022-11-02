aabcdee43426a9441a87f44b965ad38ae15bb027
ileuxali <leowendali@gmail.com>
2022-11-02 15:25:07 -0400

    user entity made, still need to replace id generator with java builtin, let me know what other getter functions or any functions are required from user. added public profile class, working on it with hannah.

---
+++ src/main/java/Entities/UserPublicProfile.java
@@ -0,0 +1,10 @@
+package Entities;
+
+import java.util.HashMap;
+
+public class UserPublicProfile {
+//    private json;
+    public UserPublicProfile(){
+//        this.preferences = HashMap<String, String> ;
+    }
+}
