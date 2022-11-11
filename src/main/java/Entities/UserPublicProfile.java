7ebf29885d00598be8649078077862e5b7d194b3
hannahpan1 <hannahpan10@gmail.com>
2022-11-11 14:42:58 -0500

    Rough Draft of Entities for User Public Profile

--- src/main/java/Entities/UserPublicProfile.java
+++ src/main/java/Entities/UserPublicProfile.java
@@ -1,10 +1,30 @@
 package Entities;
-
 import java.util.HashMap;
 
 public class UserPublicProfile {
-//    private json;
+    private String biography;
+    private HashMap<String, String> preferences;
     public UserPublicProfile(){
-//        this.preferences = HashMap<String, String> ;
+        HashMap<String, String> preferences = new HashMap<>();
+        /*Prefilling in the preferences*/
+        preferences.put("Work Hours", "");
+        preferences.put("Remote or In-person", "");
+        this.preferences = preferences;
+    }
+
+    public HashMap<String, String> getPreferences() {
+        return this.preferences;
+    }
+
+    public void setPreferences(HashMap<String, String> newPreferences) {
+        this.preferences = newPreferences;
+    }
+
+    public String getBiography() {
+        return this.biography;
+    }
+
+    public void setBiography(String bio) {
+        this.biography = bio;
     }
 }
