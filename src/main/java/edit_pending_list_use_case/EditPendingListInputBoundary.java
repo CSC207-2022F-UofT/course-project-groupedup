081f1a41900d4c3a7ed3f9049f3bb0eadc25aab9
Sharon Hsieh <sharonyt22@gmail.com>
2022-11-12 18:11:10 -0500

    Added the main functions of feature 8 (edit pending list use case). TODO: create screens and edit EditPendingListResponseModel to contain the necessary information needed for screen creation.

--- src/main/java/edit_pending_list_use_case/EditPendingListInputBoundary.java
+++
@@ -1,5 +0,0 @@
-package edit_pending_list_use_case;
-
-public interface EditPendingListInputBoundary {
-    EditPendingListResponseModel addOrRemoveUser(EditPendingListRequestModel requestModel);
-}
