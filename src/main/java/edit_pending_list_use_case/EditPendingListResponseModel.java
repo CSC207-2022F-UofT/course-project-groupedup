c153de31e17a608bafa5ae23c69f4a67219570f3
Sharon Hsieh <sharonyt22@gmail.com>
2022-11-12 18:11:10 -0500

    Added the main functions of feature 8 (edit pending list use case). TODO: create screens and edit EditPendingListResponseModel to contain the necessary information needed for screen creation.

--- src/main/java/edit_pending_list_use_case/EditPendingListResponseModel.java
+++
@@ -1,8 +0,0 @@
-package edit_pending_list_use_case;
-
-public class EditPendingListResponseModel {
-    // TODO: figure out what parameters are required to create relevant screens
-    public EditPendingListResponseModel(//parameters here) {
-
-    }
-}
--- src/main/java/edit_pending_list_use_case/PendingListPresenter.java
+++
@@ -1,7 +0,0 @@
-package edit_pending_list_use_case;
-
-public interface PendingListPresenter {
-    EditPendingListResponseModel prepareSuccessView(EditPendingListResponseModel pendingList);
-
-    EditPendingListResponseModel prepareFailView(String error);
-}
