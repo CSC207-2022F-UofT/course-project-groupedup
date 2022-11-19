3d003efc0f3b32aa47dcf6d6dba2a91b6fd2c87b
Sharon Hsieh <sharonyt22@gmail.com>
2022-11-18 20:54:05 -0500

    Fixed the other auto-grading error.

--- src/test/java/EditPendingListInteractorTest.java
+++ src/test/java/EditPendingListInteractorTest.java
@@ -1,15 +1,59 @@
+import Entities.Group;
+import Entities.User;
 import editpendinglist.*;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 public class EditPendingListInteractorTest {
 
     @Test
-    void addOrRemoveUser() {
-        PendingListDsGateway repository = new whatever;
+    public void addOrRemoveUser() {
+        EditPendingListDsGateway repository = new EditPendingListDsGateway() {
+            @Override
+            public User getUser(String username) {
+                return null;
+            }
+
+            @Override
+            public Group getGroup(String groupName) {
+                return null;
+            }
+
+            @Override
+            public HashMap<String, User> getUserMap() {
+                return null;
+            }
+
+            @Override
+            public void updateUser(String username) {
+
+            }
+
+            @Override
+            public void updateGroup(String groupName) {
+
+            }
+
+            @Override
+            public boolean userInGroup(String username, String groupName) {
+                return false;
+            }
+
+            @Override
+            public boolean groupInUser(String groupName, String username) {
+                return false;
+            }
+
+            @Override
+            public boolean userExists(String username) {
+                return false;
+            }
+        };
         EditPendingListOutputBoundary presenter = new EditPendingListOutputBoundary() {
             @Override
             public EditPendingListResponseModel prepareSuccessView(EditPendingListResponseModel pendingList) {
-                
+
                 return null;
             }
 
