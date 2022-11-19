954b7403a70fce97bc9ad8376ed6381995d2d364
soheegoo <soheegoo@gmail.com>
2022-11-19 18:02:19 -0500

    group creation use case

--- src/main/java/group_creation_screens/GroupRegisterPresenter.java
+++ src/main/java/group_creation_screens/GroupRegisterPresenter.java
@@ -3,12 +3,21 @@ import group_creation_use_case.GroupRegisterPresenter;
 import group_creation_use_case.GroupRegisterResponseModel;
 
 public class GroupRegisterResponseFormatter implements GroupRegisterPresenter{
-
+    /**
+     *
+     * @param response takes the response model data and presents it in the UI upon success
+     * @return
+     */
     @Override
     public GroupRegisterResponseModel prepareSuccessView(GroupRegisterResponseModel response) {
         return response;
     }
 
+    /**
+     *
+     * @param error takes an error message and presents a failure screen/message in the UI upon failure
+     * @return
+     */
     @Override
     public GroupRegisterResponseModel prepareFailView(String error) {
         throw new GroupCreationFailed(error);
