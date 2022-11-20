58ecaeff8be3e5ba1eba9ff324aeda8faa0ebcf0
hannahpan1 <hannahpan10@gmail.com>
2022-11-20 17:54:31 -0500

    Got rid of UseCase's DS Gateway to use Leo's.

--- src/main/java/edit_user_public_profile_usecase/EditUserPublicProfileDSGateway.java
+++
@@ -1,8 +0,0 @@
-package edit_user_public_profile_usecase;
-import Entities.NormalUser;
-
-public interface editUserPublicProfileDSGateway {
-    boolean existsByUsername(String username);
-    NormalUser findUser(String username);
-    void saveUser(editUserPublicProfileDsRequestModel requestModel);
-}
