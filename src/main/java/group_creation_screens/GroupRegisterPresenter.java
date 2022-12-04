7c4ed765301ef4a47536863d19391b7319382a36
soheegoo <soheegoo@gmail.com>
2022-12-04 18:53:11 -0500

    Made changes to get rid of exceptions

--- src/main/java/group_creation_screens/GroupRegisterPresenter.java
+++ src/main/java/group_creation_screens/GroupRegisterPresenter.java
@@ -35,6 +35,6 @@ public class GroupRegisterPresenter implements GroupRegisterOutputBoundary{
      */
     @Override
     public void prepareFailView(String error) {
-        groupRegisterScreenBoundary.prepareFailView(error);
+        this.groupRegisterScreenBoundary.prepareFailView(error);
     }
 }
